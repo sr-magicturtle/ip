41f14eab135fce48d670835ba1ef1d53d41d7ec1
sr-magicturtle <neoshaoray@gmail.com>
2026-01-22 15:29:17 +0800

    Echo user input

--- src/main/java/Roberto.java
+++ src/main/java/Roberto.java
@@ -1,20 +1,27 @@
+import java.util.Scanner;
+
 public class Roberto {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm ROBERTO");
-        robertoLogo();
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        helloGreeting();
 
-
+        Scanner scanner = new Scanner(System.in);
+        String userChoice = scanner.nextLine();
+        while (!userChoice.equals("bye")) {
+            System.out.println(userChoice);
+            userChoice = scanner.nextLine();
+        }
+        System.out.println("NOOOOO DONT GO.... okay see u soon!");
     }
 
-    private static void robertoLogo() {
+    private static void helloGreeting() {
         String logo = "__________ ________ __________________________________________________   \n" +
                 "\\______   \\\\_____  \\\\______   \\_   _____/\\______   \\__    ___/\\_____  \\  \n" +
                 " |       _/ /   |   \\|    |  _/|    __)_  |       _/ |    |    /   |   \\ \n" +
                 " |    |   \\/    |    \\    |   \\|        \\ |    |   \\ |    |   /    |    \\\n" +
                 " |____|_  /\\_______  /______  /_______  / |____|_  / |____|   \\_______  /\n" +
                 "        \\/         \\/       \\/        \\/         \\/                   \\/ ";
+        System.out.println("Hello! I'm ROBERTO");
         System.out.println(logo);
+        System.out.println("What can I do for you?");
     }
 }
