4828ed1657089fdb1afa57fd43790cb04e1b0737
sr-magicturtle <neoshaoray@gmail.com>
2026-01-22 15:14:04 +0800

    Change name to Roberto

---
+++ src/main/java/Roberto.java
@@ -0,0 +1,20 @@
+public class Roberto {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ROBERTO");
+        robertoLogo();
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+
+
+    }
+
+    private static void robertoLogo() {
+        String logo = "__________ ________ __________________________________________________   \n" +
+                "\\______   \\\\_____  \\\\______   \\_   _____/\\______   \\__    ___/\\_____  \\  \n" +
+                " |       _/ /   |   \\|    |  _/|    __)_  |       _/ |    |    /   |   \\ \n" +
+                " |    |   \\/    |    \\    |   \\|        \\ |    |   \\ |    |   /    |    \\\n" +
+                " |____|_  /\\_______  /______  /_______  / |____|_  / |____|   \\_______  /\n" +
+                "        \\/         \\/       \\/        \\/         \\/                   \\/ ";
+        System.out.println(logo);
+    }
+}
